5c9bdd44e83bad4ec8f2f194d84d35a7405d37e3
richardjones4364 <134643479+richardjones4364@users.noreply.github.com>
2023-06-11 14:48:34 +0000

    added green tick and red cross for register

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import { Container, Table, Form, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function Register() {
+  const [presentStatus, setPresentStatus] = useState([]);
+
+  const toggleStatus = (index) => {
+    setPresentStatus((prevState) => {
+      const updatedStatus = [...prevState];
+      updatedStatus[index] = !updatedStatus[index];
+      return updatedStatus;
+    });
+  };
+
   return (
-    <Container className="md-8">
+    <Container className="md-6">
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>Present</th>
+            <th colSpan={2}>Present</th>
             <th>Last</th>
             <th>First</th>
             <th>Form</th>
@@ -14,27 +27,57 @@ function Register() {
         </thead>
         <tbody>
           <tr>
-            <td>
-              <Form.Check type="checkbox" />
+            <td
+              onClick={() => toggleStatus(0)}
+              style={{ cursor: 'pointer' }}
+            >
+              <FontAwesomeIcon
+                icon={presentStatus[0] ? faCheck : faTimes}
+                className={presentStatus[0] ? 'text-success' : 'text-danger'}
+              />
             </td>
-            <td>Dave</td>
-            <td>Otto</td>
+            <td><FontAwesomeIcon
+                icon={presentStatus[0] ? faTimes : faCheck}
+                className={presentStatus[0] ? 'text-danger' : 'text-success'}
+              /></td>
+            <td>Jones</td>
+            <td>Daffydd</td>
             <td>9TH</td>
           </tr>
           <tr>
-            <td>
-              <Form.Check type="checkbox" />
+          <td
+              onClick={() => toggleStatus(0)}
+              style={{ cursor: 'pointer' }}
+            >
+              <FontAwesomeIcon
+                icon={presentStatus[0] ? faCheck : faTimes}
+                className={presentStatus[0] ? 'text-success' : 'text-danger'}
+              />
             </td>
-            <td>Jacob</td>
-            <td>Thornton</td>
+            <td><FontAwesomeIcon
+                icon={presentStatus[0] ? faTimes : faCheck}
+                className={presentStatus[0] ? 'text-danger' : 'text-success'}
+              /></td>
+            <td>Smith</td>
+            <td>Jeffy</td>
             <td>8GH</td>
           </tr>
           <tr>
-            <td>
-              <Form.Check type="checkbox" />
+          <td
+              onClick={() => toggleStatus(0)}
+              style={{ cursor: 'pointer' }}
+            >
+              <FontAwesomeIcon
+                icon={presentStatus[0] ? faCheck : faTimes}
+                className={presentStatus[0] ? 'text-success' : 'text-danger'}
+              />
             </td>
-            <td>Harry</td>
+            <td><FontAwesomeIcon
+                icon={presentStatus[0] ? faTimes : faCheck}
+                className={presentStatus[0] ? 'text-danger' : 'text-success'}
+              /></td>
             <td>Piles</td>
+            <td>Harry</td>
             <td>7GG</td>
           </tr>
         </tbody>

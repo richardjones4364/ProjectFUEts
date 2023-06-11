af41b26605b23308c029aaaf87a102a04cbfa86e
richardjones4364 <134643479+richardjones4364@users.noreply.github.com>
2023-06-11 16:47:41 +0000

    cells on register now highlight red and green when clicked.

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Container, Table, Form, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import '../App.css'; 
 
 function Register() {
   const [presentStatus, setPresentStatus] = useState([]);
@@ -16,7 +17,14 @@ function Register() {
 
   return (
     <Container className="md-6">
-      <Table striped bordered hover>
+      <Table striped bordered hover size="sm" className="table-sm">
+        <colgroup>
+          <col style={{ width: 'auto' }} />
+          <col style={{ width: 'auto' }} />
+          <col style={{ width: 'auto' }} />
+          <col style={{ width: 'auto' }} />
+          <col style={{ width: 'auto' }} />
+        </colgroup>
         <thead>
           <tr>
             <th colSpan={2}>Present</th>
@@ -29,53 +37,68 @@ function Register() {
           <tr>
             <td
               onClick={() => toggleStatus(0)}
-              style={{ cursor: 'pointer' }}
+              className={`check-cell ${presentStatus[0] ? 'selected' : ''}`}
             >
               <FontAwesomeIcon
-                icon={presentStatus[0] ? faCheck : faTimes}
-                className={presentStatus[0] ? 'text-success' : 'text-danger'}
+                icon={faCheck}
+                className={`check-icon ${presentStatus[0] ? 'text-success' : ''}`}
               />
             </td>
-            <td><FontAwesomeIcon
-                icon={presentStatus[0] ? faTimes : faCheck}
-                className={presentStatus[0] ? 'text-danger' : 'text-success'}
-              /></td>
+            <td
+              onClick={() => toggleStatus(0)}
+              className={`cross-cell ${!presentStatus[0] ? 'selected' : ''}`}
+            >
+              <FontAwesomeIcon
+                icon={faTimes}
+                className={`cross-icon ${!presentStatus[0] ? 'text-danger' : ''}`}
+              />
+            </td>
             <td>Jones</td>
             <td>Daffydd</td>
             <td>9TH</td>
           </tr>
           <tr>
-          <td
-              onClick={() => toggleStatus(0)}
-              style={{ cursor: 'pointer' }}
+            <td
+              onClick={() => toggleStatus(1)}
+              className={`check-cell ${presentStatus[1] ? 'selected' : ''}`}
             >
               <FontAwesomeIcon
-                icon={presentStatus[0] ? faCheck : faTimes}
-                className={presentStatus[0] ? 'text-success' : 'text-danger'}
+                icon={faCheck}
+                className={`check-icon ${presentStatus[1] ? 'text-success' : ''}`}
               />
             </td>
-            <td><FontAwesomeIcon
-                icon={presentStatus[0] ? faTimes : faCheck}
-                className={presentStatus[0] ? 'text-danger' : 'text-success'}
-              /></td>
+            <td
+              onClick={() => toggleStatus(1)}
+              className={`cross-cell ${!presentStatus[1] ? 'selected' : ''}`}
+            >
+              <FontAwesomeIcon
+                icon={faTimes}
+                className={`cross-icon ${!presentStatus[1] ? 'text-danger' : ''}`}
+              />
+            </td>
             <td>Smith</td>
             <td>Jeffy</td>
             <td>8GH</td>
           </tr>
           <tr>
-          <td
-              onClick={() => toggleStatus(0)}
-              style={{ cursor: 'pointer' }}
+            <td
+              onClick={() => toggleStatus(2)}
+              className={`check-cell ${presentStatus[2] ? 'selected' : ''}`}
             >
               <FontAwesomeIcon
-                icon={presentStatus[0] ? faCheck : faTimes}
-                className={presentStatus[0] ? 'text-success' : 'text-danger'}
+                icon={faCheck}
+                className={`check-icon ${presentStatus[2] ? 'text-success' : ''}`}
               />
             </td>
-            <td><FontAwesomeIcon
-                icon={presentStatus[0] ? faTimes : faCheck}
-                className={presentStatus[0] ? 'text-danger' : 'text-success'}
-              /></td>
+            <td
+              onClick={() => toggleStatus(2)}
+              className={`cross-cell ${!presentStatus[2] ? 'selected' : ''}`}
+            >
+              <FontAwesomeIcon
+                icon={faTimes}
+                className={`cross-icon ${!presentStatus[2] ? 'text-danger' : ''}`}
+              />
+            </td>
             <td>Piles</td>
             <td>Harry</td>
             <td>7GG</td>
